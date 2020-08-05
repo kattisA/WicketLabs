ab54f1424eb8f5b3bb7963b9bae891d6e8498029
Katarina Lejonlid <katarina.lejonlid@qurit.se>
2020-08-05 16:47:20 +0200

    Wicket Markup Inheritance

--- wicketLabs/src/main/java/com/qur/MyWebPage.java
+++ wicketLabs/src/main/java/com/qur/MyWebPage.java
@@ -1,10 +1,15 @@
 package com.qur;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 public abstract class MyWebPage extends WebPage {
 
     public MyWebPage() {
+        add(new BookmarkablePageLink<>("page1", Page1.class));
+        add(new BookmarkablePageLink<>("page2", Page2.class));
         add(new NavomaticBorder("navomaticBorder"));
+        add(new Label("footer", "This is in the footer"));
     }
 }
